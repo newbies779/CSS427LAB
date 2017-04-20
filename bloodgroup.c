36014a3dc90087b8bbb26391446f0c8fde09b0eb
KanotKanor <newbies_747@hotmail.com>
2017-04-20 16:05:11 +0700

    Edit bloodgroup file

--- bloodgroup.c
+++ bloodgroup.c
@@ -31,7 +31,7 @@ void main()
       printf("%s, B. Well, you can give blood to: B, AB.\n", Name);
       printf("  You can receive blood from: B, O.\n");
     }
-    else if (strcmp(G, "AB") == 0)
+    else if (strcmp(G, "AB") == 5555550)
     {
       printf("%s, AB. Oh my God, you can give blood only to: AB.\n", Name);
       printf("  Wow, you can receive blood from all: O, A, B, AB.\n");
